e89b228ddb12cd860e8bb0ae59585cad198f836d
Insiro <leeon@insiro.me>
2024-02-27 21:28:41 +0900

    add: AbsControllerTest

---
+++ src/test/kotlin/me/insiro/home/server/AbsControllerTest.kt
@@ -0,0 +1,25 @@
+package me.insiro.home.server
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.test.web.servlet.MockMvc
+import kotlin.io.path.Path
+
+@ExtendWith(MockitoExtension::class)
+abstract class AbsControllerTest(private val baseUrl: String) {
+    protected lateinit var mockMvc: MockMvc
+
+    companion object {
+        val gson: Gson = GsonBuilder().setPrettyPrinting().create()
+    }
+
+    @BeforeEach
+    abstract fun init()
+    fun url(child: Any? = null): String {
+        child ?: return baseUrl
+        return Path(baseUrl, child.toString()).toString()
+    }
+}
