b12f57a0a4ee49d72ae84c3798e4906e551a5cca
Insiro <leeon@insiro.me>
2024-02-29 23:32:09 +0900

    refactor: update UserRepository

--- src/main/kotlin/me/insiro/home/server/user/UserRepository.kt
+++ src/main/kotlin/me/insiro/home/server/user/UserRepository.kt
@@ -3,14 +3,31 @@ package me.insiro.home.server.user
 import me.insiro.home.server.application.AbsRepository
 import me.insiro.home.server.user.entity.User
 import me.insiro.home.server.user.entity.Users
+import me.insiro.home.server.user.exception.UserConflictExcept
+import me.insiro.home.server.user.exception.UserNotFoundException
 import org.jetbrains.exposed.sql.ResultRow
-import org.springframework.stereotype.Repository
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 
 
-@Repository
 class UserRepository : AbsRepository<Long, User, Users>(Users) {
     override fun new(vo: User): User {
-        TODO("Not yet implemented")
+        val id = transaction {
+            if (Users.select(Users.name).where { Users.name eq vo.name }.count() != 0L)
+                throw UserConflictExcept(vo.name)
+            Users.insertAndGetId {
+                it[name] = vo.name
+                it[email] = vo.email
+                it[password] = vo.hashedPassword
+                it[permission] = vo.permission
+                it[createdAt] = vo.createdAt
+            }
+        }
+        val updated = vo.copy()
+        updated.id = id.value
+        return updated
     }
 
     override fun relationObjectMapping(it: ResultRow): User {
@@ -19,10 +36,16 @@ class UserRepository : AbsRepository<Long, User, Users>(Users) {
         return user
     }
 
-    override fun update(vo: User): User {
-        TODO("Not yet implemented")
+    override fun update(vo: User): User = transaction {
+        val id = vo.id ?: throw UserNotFoundException(id = null)
+        Users.update {
+            this.id eq id
+            it[permission] = vo.permission
+            it[password] = vo.hashedPassword
+            it[name] = vo.name
+            it[email] = vo.email
+        }
+        findById(id)!!
     }
-
-
 }
 
--- src/test/kotlin/me/insiro/home/server/user/UserServiceTest.kt
+++ src/test/kotlin/me/insiro/home/server/user/UserServiceTest.kt
@@ -1,6 +1,5 @@
 package me.insiro.home.server.user
 
-import me.insiro.home.server.application.AbsRepository
 import me.insiro.home.server.testUtils.AbsDataBaseTest
 import me.insiro.home.server.user.dto.NewUserDTO
 import me.insiro.home.server.user.dto.UpdateUserDTO
@@ -8,14 +7,10 @@ import me.insiro.home.server.user.dto.UserRole
 import me.insiro.home.server.user.entity.User
 import me.insiro.home.server.user.entity.Users
 import me.insiro.home.server.user.exception.UserConflictExcept
-import me.insiro.home.server.user.exception.UserNotFoundException
 import me.insiro.home.server.user.utils.AuthPasswordProvider
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.insertAndGetId
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.jetbrains.exposed.sql.update
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -124,39 +119,3 @@ class UserServiceTest : AbsDataBaseTest(arrayListOf(Users)) {
     }
 }
 
-class UserRepository : AbsRepository<Long, User, Users>(Users) {
-    override fun new(vo: User): User {
-        val id = transaction {
-            if (Users.select(Users.name).where { Users.name eq vo.name }.count() != 0L)
-                throw UserConflictExcept(vo.name)
-            Users.insertAndGetId {
-                it[name] = vo.name
-                it[email] = vo.email
-                it[password] = vo.hashedPassword
-                it[permission] = vo.permission
-                it[createdAt] = vo.createdAt
-            }
-        }
-        val updated = vo.copy()
-        updated.id = id.value
-        return updated
-    }
-
-    override fun relationObjectMapping(it: ResultRow): User {
-        val user = User(it[Users.name], it[Users.password], it[Users.email], it[Users.permission])
-        user.id = it[Users.id].value
-        return user
-    }
-
-    override fun update(vo: User): User = transaction {
-        val id = vo.id ?: throw UserNotFoundException(id = null)
-        Users.update {
-            this.id eq id
-            it[permission] = vo.permission
-            it[password] = vo.hashedPassword
-            it[name] = vo.name
-            it[email] = vo.email
-        }
-        findById(id)!!
-    }
-}
