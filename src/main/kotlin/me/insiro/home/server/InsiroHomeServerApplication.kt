3c1c1d9283450fdcf1fc36ee26a88ac9d5147a06
Insiro <leeon@insiro.me>
2024-03-02 17:59:59 +0900

    add: Configuration for FileRepository

--- src/main/kotlin/me/insiro/home/server/InsiroHomeServerApplication.kt
+++ src/main/kotlin/me/insiro/home/server/InsiroHomeServerApplication.kt
@@ -3,8 +3,10 @@ package me.insiro.home.server
 import org.jetbrains.exposed.spring.autoconfigure.ExposedAutoConfiguration
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 @ImportAutoConfiguration(ExposedAutoConfiguration::class)
 class InsiroHomeServerApplication
