e44de30fdce07f305e0d67509302a4a653d21ebc
Insiro <leeon@insiro.me>
2024-03-03 23:15:27 +0900

    fix: uri resolve error

--- src/test/kotlin/me/insiro/home/server/testUtils/AbsControllerTest.kt
+++ src/test/kotlin/me/insiro/home/server/testUtils/AbsControllerTest.kt
@@ -2,11 +2,11 @@ package me.insiro.home.server.testUtils
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import me.insiro.home.server.application.domain.EntityVO
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.test.web.servlet.MockMvc
-import java.net.URI
 
 @ExtendWith(MockitoExtension::class)
 abstract class AbsControllerTest(private val baseUrl: String) {
@@ -23,6 +23,7 @@ abstract class AbsControllerTest(private val baseUrl: String) {
         get() {return baseUrl}
     fun uri(child: Any? = null): String {
         child ?: return baseUrl
-        return URI.create(baseUrl).resolve(child.toString()).toString()
+        val childString = if( child is EntityVO.Id<*>)child.value.toString() else child.toString()
+        return "$baseUrl/$childString"
     }
 }
