bbdf05555f82e02ce1d0380ccf9315429eaae42f
Insiro <leeon@insiro.me>
2024-03-28 20:12:17 +0900

    refactor: ProjectDomains refactor: ProjectController return Project Type refactor: Project can Create with Specified ID

--- src/main/kotlin/me/insiro/home/server/project/dto/project/NewProjectDTO.kt
+++ src/main/kotlin/me/insiro/home/server/project/dto/project/NewProjectDTO.kt
@@ -1,10 +1,12 @@
 package me.insiro.home.server.project.dto.project
 
 import me.insiro.home.server.application.domain.entity.Status
+import me.insiro.home.server.project.entity.Project
 
 data class NewProjectDTO(
     val title: String,
     val status: Status?,
     val content: String,
-    val types: List<String>?
+    val types: List<String>?,
+    val id: Project.Id? // Create With Specified ID
 )
